b07b866cb7e41e3e9a5dec90b13670b9f51379a5
y6367 <yjoe2035@gmail.com>
2024-01-10 12:04:51 -0800

    Moves are logged in an array

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 const section = document.querySelectorAll('.section')
 let temp = 0
+let gameboard = ['', '', '',
+                 '', '', '',
+                 '', '', '']
+let gameIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+let userGame = []
+let computerGame = []
+
 section.forEach((spot) => {
     spot.dataset.index = temp
     spot.addEventListener('click', () => {
         spot.innerHTML = 'X'
         gameboard[spot.dataset.index] = 'x'
         gameIndexes[spot.dataset.index] = ''
+        userGame.push(Number(spot.dataset.index))
+        userGame.sort((a, b) => a - b)
+        console.log(userGame)
 
         while (true) {
             let check = Math.floor(Math.random() * (8 + 1))
-            if (gameIndexes[check] != '' && gameIndexes[check] != '') {
+            if (gameIndexes[check] != '') {
                 gameIndexes[check] = ''
                 gameboard[check] = 'o'
+
+                computerGame.push(Number(check))
+                computerGame.sort((a, b) => a - b)
+                console.log(computerGame)
                 let tempSpot = document.querySelector(`.${CSS.escape(check)}`);
                 tempSpot.innerHTML = 'O'
-                break;
+                break
+            }
+            const allFull = gameIndexes.every(item => item === '')
+            if (allFull) {
+                break
             }
         }
+
         console.log(gameboard)
     })
     console.log(spot.dataset.index)
     temp++
 })
-
-
-let gameboard = ['', '', '',
-                 '', '', '',
-                 '', '', '']
-let gameIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-
-function addUser(index) {
-    
-}
